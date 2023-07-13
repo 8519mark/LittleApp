8632677c88fc8c89ab403ae59e0ab7533a363f82
MarkTsai <mark0975272753@gmail.com>
2023-07-13 16:26:47 -0700

    color change on buttom click implemented, pins can be selected

--- src/components/map/Pin.tsx
+++ src/components/map/Pin.tsx
@@ -1,5 +1,6 @@
 import './PinStyle.css'
-import { MarkerF } from '@react-google-maps/api';
+import { GoogleMap } from '@react-google-maps/api';
+import { Marker } from '@react-google-maps/api';
 
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
@@ -17,35 +18,27 @@ type props_Pin = {
     doSelect : (id : number) => void
 }
 
-const selectedColor = '#007bff'
+const selectedColor = '#ff0000'
 const notSelectedColor = '#ced4da'
-
-const selectedIcon = {
-    path : faLocationDot.icon[4] as string, fillColor: selectedColor, 
-    fillOpacity: 1, anchor: new google.maps.Point(faLocationDot.icon[0] / 2, faLocationDot.icon[1]), strokeWeight: 1,
-    strokeColor: "#000000", scale: 0.080
-}
-const notSelectedIcon = {
-    path : faLocationDot.icon[4] as string, fillColor: notSelectedColor, 
-    fillOpacity: 1, anchor: new google.maps.Point(faLocationDot.icon[0] / 2, faLocationDot.icon[1]), strokeWeight: 1,
-    strokeColor: "#000000", scale: 0.080
-}
-
 
 
 export const Pin = (props : props_Pin) => {
-    const [icon, setIcon] = useState(props.pin.selected? selectedIcon : notSelectedIcon);
+    const [color, setColor] = useState(props.pin.selected? '#007bff' : '#ced4da');
 
     const handleSelect = () =>{
         props.doSelect(props.pin.id)
         // change to select/notSelect color here
-        const newIcon = props.pin.selected ? selectedIcon : notSelectedIcon;
-        setIcon(newIcon);
-        console.log(newIcon.fillColor)
+        const newColor = props.pin.selected ? selectedColor : notSelectedColor;
+        setColor(newColor);
+        console.log(newColor)
     }
 
     return (
-        <MarkerF position = {props.pin.center} key = {props.pin.id} icon = {icon}
+        <Marker position = {props.pin.center} key = {props.pin.id} icon = {{path : faLocationDot.icon[4] as string, 
+            fillColor: color, 
+            fillOpacity: 1, anchor: new google.maps.Point(faLocationDot.icon[0] / 2, faLocationDot.icon[1]), strokeWeight: 1,
+            strokeColor: "#000000", scale: 0.080
+        }}
             onClick={handleSelect}/>
     );
 };
